cad6dcd8b701555618c0c1049fc7c79e0a8611ed
flo1234514 <157308454+flo1234514@users.noreply.github.com>
2024-06-07 15:30:45 +0100

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,5 +1,5 @@
 const supabaseUrl = 'https://deroqcukulkdrwqtpqzh.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOi';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRlcm9xY3VrdWxrZHJ3cXRwcXpoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTc3Njg2OTcsImV4cCI6MjAzMzM0NDY5N30.glgZLXA4iFCmG20xK_XIpnXot9jYCUvcYKu_8xjJJLQ';
 const supabase = supabase.createClient(supabaseUrl, supabaseKey);
 
 export { supabase };
