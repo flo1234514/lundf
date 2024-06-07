ba445496e010b64029ea1d815b6e4461f42d98d6
flo1234514 <157308454+flo1234514@users.noreply.github.com>
2024-06-07 15:21:11 +0100

    Create supabase.js

---
+++ supabase.js
@@ -0,0 +1,5 @@
+const supabaseUrl = 'https://deroqcukulkdrwqtpqzh.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOi';
+const supabase = supabase.createClient(supabaseUrl, supabaseKey);
+
+export { supabase };
